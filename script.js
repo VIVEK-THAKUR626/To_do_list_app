b658736d6b8dcea78c39621c65453c5634f65cab
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2025-08-03 19:49:44 +0530

    add feature to mark a task completed or uncompleted

--- script.js
+++ script.js
@@ -3,26 +3,113 @@ const enterTask = document.getElementById("enterTask");
 const toDoList = document.querySelector(".list")
 
 const taskArray = [];
+const checkArray = [];
+
 let taskIndex = 0;
 
 addTask.addEventListener("click",()=>{
     if(enterTask.value.trim() != ""){
+        const taskDiv = document.createElement('div');
+        taskDiv.className = `${taskIndex}`;
+
+        taskDiv.style.display = "flex";
+        taskDiv.style.justifyContent = "space-around";
+        taskDiv.style.alignItems = "center";
+        taskDiv.style.borderBottom = "3px solid black";
+        taskDiv.style.margin = "0 1em 0 1em";
+
         const task = document.createElement('li');
-        task.className = `task${taskIndex}`;
 
         task.style.width = "60%";
         task.style.height = "2em";
-        task.style.margin = "1em 0 0 0.75em";
         task.style.fontSize = "1.75em";
+        task.style.margin = "0.35em 0 0 0";
         task.textContent = enterTask.value;
 
-        taskArray.push(task);
+        const checkbox = document.createElement('div');
+
+        checkbox.className = "taskPending";
+        checkbox.style.backgroundColor = "black";
+        checkbox.style.height = "2em";
+        checkbox.style.width = "10%";
+        checkbox.style.border = "1.5px solid black";
+        checkbox.style.borderRadius = "10px";
+
+        checkbox.addEventListener("click",(event)=>{
+            if(event.currentTarget.className === "taskCompleted"){
+                event.currentTarget.innerHTML = ``;
+                event.currentTarget.className = "taskPending"
+            }else{
+                event.currentTarget.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" height="2.1em" width="2.1em"><path fill="#26d317ff" d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z"/></svg>`;
+                event.currentTarget.className = "taskCompleted";
+            }
+        })
+
+        taskDiv.appendChild(task);
+        taskDiv.appendChild(checkbox);
+
+        taskArray.push(taskDiv);
+        checkArray.push(checkbox);
+
         taskIndex += 1;
 
-        toDoList.appendChild(task);
+        toDoList.appendChild(taskDiv);
     }else{
         alert("ENTER A TASK");
     }
 })
 
-console.log(taskArray)
+enterTask.addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+        if(enterTask.value.trim() != ""){
+        const taskDiv = document.createElement('div');
+        taskDiv.className = `${taskIndex}`;
+
+        taskDiv.style.display = "flex";
+        taskDiv.style.justifyContent = "space-around";
+        taskDiv.style.alignItems = "center";
+        taskDiv.style.borderBottom = "3px solid black";
+        taskDiv.style.margin = "0 1em 0 1em";
+
+        const task = document.createElement('li');
+
+        task.style.width = "60%";
+        task.style.height = "2em";
+        task.style.fontSize = "1.75em";
+        task.style.margin = "0.35em 0 0 0";
+        task.textContent = enterTask.value;
+
+        const checkbox = document.createElement('div');
+
+        checkbox.className = "taskPending";
+        checkbox.style.backgroundColor = "black";
+        checkbox.style.height = "2em";
+        checkbox.style.width = "10%";
+        checkbox.style.border = "1.5px solid black";
+        checkbox.style.borderRadius = "10px";
+
+        checkbox.addEventListener("click",(event)=>{
+            if(event.currentTarget.className === "taskCompleted"){
+                event.currentTarget.innerHTML = ``;
+                event.currentTarget.className = "taskPending"
+            }else{
+                event.currentTarget.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" height="2.1em" width="2.1em"><path fill="#6acd18" d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z"/></svg>`;
+                event.currentTarget.className = "taskCompleted";
+            }
+        })
+
+        taskDiv.appendChild(task);
+        taskDiv.appendChild(checkbox);
+
+        taskArray.push(taskDiv);
+        checkArray.push(checkbox);
+
+        taskIndex += 1;
+
+        toDoList.appendChild(taskDiv);
+    }else{
+        alert("ENTER A TASK");
+    }
+    }
+    
+})
